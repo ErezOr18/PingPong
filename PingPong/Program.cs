5ca4db9673a8617ed4e92677f89a9b760e5d730d
Erez Or <erez180103@gmail.com>
2022-01-19 17:23:41 +0200

    added sample msdn client and tested working

--- PingPong/Program.cs
+++ PingPong/Program.cs
@@ -16,14 +16,14 @@ namespace PingPong
             {
                 port = 11000;
             }
-            Console.WriteLine("Erez");
             var read = new PingPong.Server.PongReadSocket();
             var send = new PingPong.Server.PongSendSocket();
             IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = ipHostInfo.AddressList[0];
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
             var server = new PingPong.Server.Server(localEndPoint,send,read);
-            server.StartListening().Wait();
+            server.StartListening();
+            PingPong.Client.AsynchronousClient.StartClient();
         }
     }
 }
